f2b2fd5c09ddbf1c99c307ea9eb6bf797dc8462c
samirghouri <samira.ghouri@st.niituniversity.in>
2020-09-07 14:16:18 +0530

    Updates

---
+++ precache-manifest.3f6487e336b889b19cfb55abb337af3f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48106e2fa397139761d6b4490a4bf465",
+    "url": "/Covid-19-Tracker/index.html"
+  },
+  {
+    "revision": "48447dbd9a3c3849fb8a",
+    "url": "/Covid-19-Tracker/static/css/main.0f91aba2.chunk.css"
+  },
+  {
+    "revision": "51ca3ed53ffb1968114e",
+    "url": "/Covid-19-Tracker/static/js/2.038734a8.chunk.js"
+  },
+  {
+    "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
+    "url": "/Covid-19-Tracker/static/js/2.038734a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48447dbd9a3c3849fb8a",
+    "url": "/Covid-19-Tracker/static/js/main.2707ce42.chunk.js"
+  },
+  {
+    "revision": "58f575d69cd91d6d9813",
+    "url": "/Covid-19-Tracker/static/js/runtime-main.1d643bc6.js"
+  }
+]);
